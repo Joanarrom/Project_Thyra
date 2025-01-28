9f7289a25003e20a14c78c4d528144956db53bae
Joanarrom <rakdos23yt@gmail.com>
2025-01-28 12:52:41 +0100

    Fixed Arrow Bug

--- Assets/Scripts/Tutorial/UI/ArrowSpawner.cs
+++ Assets/Scripts/Tutorial/UI/ArrowSpawner.cs
@@ -7,14 +7,38 @@ public class ArrowSpawner : MonoBehaviour
     public GameObject arrowPrefab; 
     public Transform[] spawnPoints; 
     public Transform player; 
+    public float spawnDelay = 1f; 
+
+    private int currentIndex = 0; 
 
     void Start()
     {
         
-        foreach (Transform point in spawnPoints)
+        StartCoroutine(SpawnArrows());
+    }
+
+    IEnumerator SpawnArrows()
+    {
+        
+        while (currentIndex < spawnPoints.Length)
         {
+            
+            Transform point = spawnPoints[currentIndex];
+
+            
             GameObject arrow = Instantiate(arrowPrefab, point.position, Quaternion.identity);
+
+            
             arrow.GetComponent<ArrowIndicator>().player = player;
+
+           
+            yield return new WaitUntil(() => arrow == null); 
+
+            
+            currentIndex++;
+            
+           
+            yield return new WaitForSeconds(spawnDelay);
         }
     }
 }
